11ae0b7360b07ec90684fd89f6a178fe1380701f
Scr0ol <wendellamaral424@gmail.com>
2024-05-24 00:51:56 -0300

    primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress"/>
+
+describe('Test login', () => {
+    it('deve realizar o login com sucesso', () => {
+        cy.visit("cadastro/cadastro.html ")
+        cy.get('#nome').type("ramongay")
+        cy.get('#senha').type("willa1")
+        cy.get('#cpf').type("00000000000")
+        cy.get('#email').type("willacabeçadebalde@gmail.com")
+        cy.get('#data').type("01/10/1999")
+        cy.get('#cadastrar').click()
+        cy.get('#cpf').type("00000000000")
+        cy.get('#senha').type("willa1")
+        cy.get('#login').click()
+        
+
+    });
+});
--- login/login.js
+++ login/login.js
@@ -1,33 +1,37 @@
-let usuarios = []
-if (localStorage.getItem('cadastrados')) 
-    usuarios = JSON.parse(localStorage.getItem('cadastrados'))
-function login(){
-    let guarda_senha = document.getElementById('senha').value
-    let guarda_cpf = document.getElementById('cpf').value
-    if(procura_usuario(guarda_cpf, guarda_senha) != -1){
-        alert("Usuário validado! Bem-vindo!")
-        location.assign('file:///C:/Users/mcmiq/Documents/Codiguin/AV2%20-%20Teste%20de%20software/pages/qrcode.html')
-    }else{
-        alert("Usuário não cadastrado")
-    }
-}
-function procura_usuario(guarda_cpf, guarda_senha,) {
-    let index = usuarios.findIndex((element) => {
-        return element.cpf == guarda_cpf && element.senha == guarda_senha
-    })
-    return index
+let usuarios = [];
+if (localStorage.getItem('cadastrados')) {
+    usuarios = JSON.parse(localStorage.getItem('cadastrados'));
 }
 
-let btn_senha = document.getElementById('senha')
-let btn_cpf = document.getElementById('cpf')
+function login() {
+    const guarda_senha = document.getElementById('senha').value;
+    const guarda_cpf = document.getElementById('cpf').value;
+    const userIndex = procura_usuario(guarda_cpf, guarda_senha);
+    if (userIndex!== -1) {
+        alert("Usuário validado! Bem-vindo!");
+        location.assign('.//qrcode.html');
+    } else {
+        alert("Usuário não cadastrado");
+    }
+}
 
-btn_cpf.addEventListener('keypress', (event) => {
-    if (event.key == "Enter") {
+function procura_usuario(guarda_cpf, guarda_senha) {
+    return usuarios.findIndex((element) => {
+        return element.cpf === guarda_cpf && element.senha === guarda_senha;
+    });
+}
+
+const btn_senha = document.getElementById('senha');
+const btn_cpf = document.getElementById('cpf');
+
+btn_cpf.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
         login();
     }
-})
-btn_senha.addEventListener('keypress', (event) => {
-    if (event.key == "Enter") {
+});
+
+btn_senha.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
         login();
     }
-})
+});
